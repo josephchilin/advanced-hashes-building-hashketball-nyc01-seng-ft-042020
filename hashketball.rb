455a101b5a8147168b290820e214e497d52999c4
Jo L <joseph@josephchilin.com>
2020-04-16 21:14:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -26,6 +26,16 @@ def game_hash
         :slam_dunks => 7 },
         
         {:player_name => "Brook Lopez",
+        :number => 11,
+        :shoe => 17,
+        :points => 17,
+        :rebounds => 19,
+        :assists => 10,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 15 },
+        
+        {:player_name => "Mason Plumlee",
         :number => 1,
         :shoe => 19,
         :points => 26,
@@ -34,16 +44,6 @@ def game_hash
         :steals => 3,
         :blocks => 8,
         :slam_dunks => 5 },
-        
-        {:player_name => "Mason Plumlee",
-        :number => 0,
-        :shoe => 16,
-        :points => 22,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 3,
-        :blocks => 1,
-        :slam_dunks => 1 },
         
         {:player_name => "Jason Terry",
         :number => 0,
